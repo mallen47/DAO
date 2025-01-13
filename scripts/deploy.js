e300245081ab47930346fc956ce2f46c32c2775e
Matt Allen <allen.matt@gmail.com>
2025-01-13 06:55:56 -0500

    setup project from template

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,19 +4,19 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
-  const MAX_SUPPLY = '1000000'
+  const NAME = 'Dapp University';
+  const SYMBOL = 'DAPP';
+  const MAX_SUPPLY = '1000000';
 
   // Deploy Token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  const Token = await hre.ethers.getContractFactory('Token');
+  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY);
 
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  await token.deployed();
+  console.log(`Token deployed to: ${token.address}\n`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
